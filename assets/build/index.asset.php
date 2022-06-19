632a538313829a399bda33d897858a71df587b8c
kermage <genealyson.torcende@gmail.com>
2022-06-19 14:58:47 +0800

    normalize fields spacing

--- assets/build/index.asset.php
+++ assets/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-server-side-render'), 'version' => '1930532cca15f8cb0e5c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-server-side-render'), 'version' => '3da5035df93663a75cb4');
