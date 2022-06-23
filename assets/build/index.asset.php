3b4e026c201995af87d7a6e2a676b9678632eb75
kermage <genealyson.torcende@gmail.com>
2022-06-23 21:58:10 +0800

    first stable release

--- assets/build/index.asset.php
+++ assets/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-server-side-render'), 'version' => '3da5035df93663a75cb4');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-server-side-render'), 'version' => '557d97831c3dacd6ab82');
