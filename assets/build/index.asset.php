54ccfe269b69cdb823ca424d16e119f614428d03
kermage <genealyson.torcende@gmail.com>
2023-02-16 22:08:24 +0800

    update assets dependencies

--- assets/build/index.asset.php
+++ assets/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-server-side-render'), 'version' => 'c909206432b325798ee8');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-server-side-render'), 'version' => '2ec86794f52f48c7d1a2');
