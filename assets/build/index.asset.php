dcf88544346792e02d1467edd48059a0d29310d8
kermage <genealyson.torcende@gmail.com>
2023-05-09 22:09:49 +0800

    do not show the appender during content load

--- assets/build/index.asset.php
+++ assets/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-server-side-render'), 'version' => 'e4f8170954e1e206c5e2');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-server-side-render'), 'version' => '30c48dfe27619ee56070');
