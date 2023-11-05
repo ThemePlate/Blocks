50cd8b6f6a4db21c2edea443912395b5bab29bab
kermage <genealyson.torcende@gmail.com>
2023-11-05 23:16:22 +0800

    release with production build

--- assets/build/index.asset.php
+++ assets/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-server-side-render'), 'version' => '44895de4e49dafe7945e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-server-side-render'), 'version' => '7e79a8f731c3ec151317');
